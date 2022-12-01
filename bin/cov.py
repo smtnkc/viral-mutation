cdc49ae1e321ffb57f4fece44375bc7a42d98d1e
smtnkc <smtnkc@gmail.com>
2022-12-01 20:35:01 +0300

    typo in cov.py

--- bin/cov.py
+++ bin/cov.py
@@ -198,16 +198,17 @@ def plot_umap(adata, categories, namespace='cov'):
 
 def analyze_embedding(args, model, seqs, vocabulary):
 
-    if os.path.isfile('embed_seq.pickle'):
+    if os.path.isfile('embed_seqs.pickle'):
         tprint('Loading embed_seqs.pickle...')
         with open('embed_seqs.pickle', 'rb') as handle:
             seqs = pickle.load(handle)
+        tprint('Successfully loaded embed_seqs')
     else:
         seqs = embed_seqs(args, model, seqs, vocabulary, use_cache=False)
-        tprint('Saving embed_seq as pickle file...')
+        tprint('Saving embed_seqs as pickle file...')
         with open('embed_seqs.pickle', 'wb') as handle:
             pickle.dump(seqs, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        tprint('Successfully saved embed_seq.pickle')
+        tprint('Successfully saved embed_seqs.pickle')
 
     X, obs = [], {}
     obs['n_seq'] = []
